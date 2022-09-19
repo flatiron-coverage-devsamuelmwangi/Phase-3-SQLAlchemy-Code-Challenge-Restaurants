d43360a17fc60078b3ef5a2c99b35cba7227b467
Prabhdip Gill <prabhdip.gill@flatironschool.com>
2022-09-19 01:53:05 -0700

    Add migrations and update readme

---
+++ lib/migrations/versions/3204bcc6d697_first_migration.py
@@ -0,0 +1,24 @@
+"""First migration
+
+Revision ID: 3204bcc6d697
+Revises: 
+Create Date: 2022-09-19 01:36:03.462170
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '3204bcc6d697'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
--- lib/models.py
+++ lib/models.py
@@ -9,17 +9,28 @@ from sqlalchemy.ext.declarative import declarative_base
 
 
 Base = declarative_base()
+engine = create_engine('sqlite:///db/restaurants.db', echo=True)
 
-engine = create_engine('sqlite:///restaurants.db', echo=True)
 
-class Restaurants(Base):
-    pass
+# class Review(Base):
+#     pass
 
-class Review(Base):
-    pass
+class Restaurant(Base):
+    __tablename__ = 'restaurants'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String())
+    price = Column(Integer)
+
+    def __repr__(self):
+        return f'Restaurant: {self.name}'
 
 class Customer(Base):
-    pass
+    __tablename__ = 'customers'
 
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String())
+    last_name = Column(String())
 
-Base.metadata.create_all(bind=engine)
+    def __repr__(self):
+        return f'Customer: {self.name}'
